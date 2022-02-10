0cf1aba2ea29124d9c80279349410b3b93e2ff11
Arpitpundir <thakurarpitpundir73@gmail.com>
2022-02-10 11:58:09 +0530

    Solved Printing LCS

---
+++ LCS-based-problems/Printing-LCS.cpp
@@ -0,0 +1,19 @@
+void printLcs(vector<vector<int> > dp, string a, string b){
+    int row = dp.size()-1, col = dp[0].size()-1;
+    vector<char> ans;
+    while(row && col){
+        if(a[row-1] == b[col-1]){
+            ans.push_back(a[row-1]);
+            row--, col--;
+        }else if(dp[row-1][col]<dp[row][col-1]){
+            col--;
+        }else {
+            row--;
+        }
+    }
+    reverse(ans.begin(),ans.end());
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i];
+    }
+    cout<<endl;
+}
